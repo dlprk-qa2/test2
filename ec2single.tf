cc529a2ab596ce26f74e0700f46233f1efd763ab
dlprk-qa2 <101082466+dlprk-qa2@users.noreply.github.com>
2022-07-05 17:22:45 +0530

    Update ec2single.tf

--- ec2single.tf
+++ ec2single.tf
@@ -15,7 +15,7 @@ provider "aws" {
 
  #text12
  
-resource "aws_instance" "app_server" {
+resource "aws_instance" "app_server2" {
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
   ingress_cidr_blocks = ["0.0.0.0/16"]
