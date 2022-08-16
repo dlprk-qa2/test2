37ed977307c08e9f0f5c82ddf8f7f72fbdbbd271
dlprk-qa2 <101082466+dlprk-qa2@users.noreply.github.com>
2022-08-16 16:03:12 +0530

    Update ec2single.tf

--- ec2single.tf
+++ ec2single.tf
@@ -15,7 +15,7 @@ provider "aws" {
 
  #text12
  
-resource "aws_instance" "app_server2" {
+resource "aws_instance" "app_server" {
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
   tags = {
