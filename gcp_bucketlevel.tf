bde8ec99055021939b6e11209e54d3e3aa439b18
dlprk-qa2 <101082466+dlprk-qa2@users.noreply.github.com>
2022-08-16 12:01:42 +0530

    Delete gcp_bucketlevel.tf

--- gcp_bucketlevel.tf
+++
@@ -1,8 +0,0 @@
-provider "google" {
- region="us.west"
-}
-resource "google_storage_bucket" "Bucketlevel" {
-    name     = "TestBucket"
-    bucket_policy_only = true
-    uniform_bucket_level_access = false
-}
