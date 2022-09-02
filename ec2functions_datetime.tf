9ee8dff981675def307278d69014bb09631eb7bd
dlprk-qa2 <101082466+dlprk-qa2@users.noreply.github.com>
2022-09-02 18:34:32 +0530

    Add files via upload

---
+++ ec2functions_datetime.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+}
+ # required_version = ">= 0.14.9"
+
+provider "aws" {
+  profile = var.profile 
+  region  = var.region
+}
+
+ #text
+ 
+resource "aws_instance" "app_server" {
+  ami           = trim("?!hello?!", "!?")
+  count         = ceil(2.3)
+   
+  instance_type = chomp("t2.micro\n")
+  tags = {
+    Name = formatdate("DD MMM YYYY hh:mm ZZZ", "2018-01-02T23:12:01Z")
+    Name1 = timeadd("2017-11-22T00:00:00Z", "10m") 
+    Name2 = timestamp()
+    Name3 = formatdate("DD MMM YYYY hh:mm ZZZ", )
+    Name4 = formatdat("DD MMM YYYY hh:mm ZZZ","2018-01-02T23:12:01Z" )
+    Name5 = timeadd("2017-11-22T00:00:00Z", ) 
+    
+}
+}
