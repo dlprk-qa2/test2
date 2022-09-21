c0bf66ab2c77d0c0fdee779b2e5bc3e9f8e9ab5b
dlprk-qa2 <101082466+dlprk-qa2@users.noreply.github.com>
2022-09-21 16:28:02 +0530

    Update s3_versioning.tf

--- s3_versioning.tf
+++ s3_versioning.tf
@@ -6,8 +6,8 @@ resource "aws_s3_bucket" "s3_versioning1" {
   }
 }
 
-module "iam_example_iam-account" {
-  source  = "terraform-aws-modules/iam/aws//examples/iam-account"
-  version = "5.4.0"
+module "vpc_example_complete-vpc" {
+  source  = "terraform-aws-modules/vpc/aws//examples/complete-vpc"
+  version = "3.14.4"
 }
 
