7b36bd13954def11123193638a132e99dac31c3f
dlprk-qa2 <101082466+dlprk-qa2@users.noreply.github.com>
2022-09-21 12:03:15 +0530

    Update s3_versioning.tf

--- s3_versioning.tf
+++ s3_versioning.tf
@@ -6,7 +6,7 @@ resource "aws_s3_bucket" "s3_versioning" {
   }
 }
 
-module "s3-bucket_example_complete" {
-  source  = "terraform-aws-modules/s3-bucket/aws//examples/complete"
-  version = "3.4.0"
+module "elb_example_complete" {
+  source  = "terraform-aws-modules/elb/aws//examples/complete"
+  version = "3.0.1"
 }
