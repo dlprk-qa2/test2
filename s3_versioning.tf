d9259ac81609b1068a0f415335e1d27b5246da92
dlprk-qa2 <101082466+dlprk-qa2@users.noreply.github.com>
2022-09-21 08:01:42 +0530

    Update s3_versioning.tf

--- s3_versioning.tf
+++ s3_versioning.tf
@@ -5,3 +5,8 @@ resource "aws_s3_bucket" "s3_versioning" {
     status = "Enabled"
   }
 }
+
+module "s3-bucket_example_complete" {
+  source  = "terraform-aws-modules/s3-bucket/aws//examples/complete"
+  version = "3.4.0"
+}
