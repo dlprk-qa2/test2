f41974c15c1e353cbcba43985a4214d977200cb7
dlprk-qa2 <101082466+dlprk-qa2@users.noreply.github.com>
2022-09-23 09:56:28 +0530

    Update s3_versioning.tf

--- s3_versioning.tf
+++ s3_versioning.tf
@@ -5,3 +5,21 @@ resource "aws_s3_bucket" "s3_versioning" {
     status = "Enabled"
   }
 }
+module "dynamodb-table" {
+  source    = "terraform-aws-modules/dynamodb-table/aws"
+  version   = "3.1.1"
+  name      = "testtable"
+  hash_key  = "pk"
+  range_key = "sk"
+
+  attributes = [
+    {
+      name = "pk"
+      type = "S"
+    },
+    {
+      name = "sk"
+      type = "S"
+    }
+  ]
+}
