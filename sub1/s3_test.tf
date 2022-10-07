2833505966e36cf6068d7c0787fff7c98ce1505b
dlprk-qa2 <101082466+dlprk-qa2@users.noreply.github.com>
2022-10-07 15:29:18 +0530

    Update s3_test.tf

--- sub1/s3_test.tf
+++ sub1/s3_test.tf
@@ -5,3 +5,17 @@ resource "aws_s3_bucket" "logging2" {
     enabled    = true
   }
 }
+
+module "vpc_example_complete-vpc" {
+  source  = "terraform-aws-modules/vpc/aws//examples/complete-vpc"
+  version = "3.14.4"
+}
+module "iam_example_iam-account" {
+  source  = "terraform-aws-modules/iam/aws//examples/iam-account"
+  version = "5.4.0"
+}
+module "security-group_example_complete" {
+  source  = "terraform-aws-modules/security-group/aws//examples/complete"
+  version = "4.13.0"
+}  
+  
