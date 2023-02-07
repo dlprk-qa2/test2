10c3b1a6c6e35795cf8bf93b4af86f0589655bf8
dlprk-qa2 <101082466+dlprk-qa2@users.noreply.github.com>
2023-02-07 19:45:41 +0530

    Add files via upload

---
+++ ec2single.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+}
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+}
+resource "aws_instance" "app_server" {
+  #zpc-skip-policy:ZS-AWS-00029:Please add a comment
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+  ingress_cidr_blocks = ["0.0.0.0/16"]
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
